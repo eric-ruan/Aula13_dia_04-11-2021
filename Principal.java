841745fa9ce63f5843c8bd7e82bef079cbf734e4
eric-ruan <ericrss2021@gmail.com>
2021-11-04 19:17:59 +0000

    Alterações para colocar o INput ou Scanner

--- Principal.java
+++ Principal.java
@@ -1,20 +1,31 @@
-
+import java.util.Scanner;
 
 public class Principal {
     public static void main(String[] args) {
         Aluno a = new Aluno();
         Professor p = new Professor();
 
+        //Scanner/ input
+        Scanner sc = new Scanner(System.in);
+
+
         //Dados do aluno
-        a.setNome("Éric Ruan");
-        a.setEndereco("Rua do limão");
-        a.setTelefone("14-99988-7766");
-        a.setCpf("111.222.333-44");
+        System.out.println("Digite seu nome:");
+        a.setNome(sc.nextLine());
+        System.out.println("Digite seu endereço:");
+        a.setEndereco(sc.nextLine());
+        System.out.println("Digite seu telefone:");
+        a.setTelefone(sc.nextLine());
+        System.out.println("Digite seu CPF:");
+        a.setCpf(sc.nextLine());
 
         //Notas do aluno
-        a.setCursos("BigData no Agronegócio");
-        a.setNota1(9.0);
-        a.setNota2(8.0);
+        System.out.println("Digite seu curso:");
+        a.setCursos(sc.nextLine());
+        System.out.println("Digite a primeira nota:");
+        a.setNota1(sc.nextInt());
+        System.out.println("Digite a segunda nota:");
+        a.setNota2(sc.nextInt());
         
         //Prints
         System.out.println("O aluno: ");
@@ -25,9 +36,9 @@ public class Principal {
         System.out.println(a.getCursos());
 
         if(a.verificarAprovado()){
-            System.out.println("Aprovado");
+            System.out.println(a.calcularMedia());
         }else{
-            System.out.println("Reprovado");
+            System.out.println(a.calcularMedia());
         }
 
     }
